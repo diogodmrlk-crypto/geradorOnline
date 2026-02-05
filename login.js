f2315227d018919c36e107d2ccdc7f638bc11e9e
diogodmrlk-crypto <diogodmrlk@gmail.com>
2026-02-05 16:15:10 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,9 @@
 import fs from "fs";
 
-export default function handler(req, res) {
+export default function handler(req, res){
   const { username, password } = req.body;
-
   const data = JSON.parse(fs.readFileSync("data.json"));
-
   const user = data.users.find(u => u.username === username && u.password === password);
-
-  if (!user) return res.status(401).json({ error: "Login inválido" });
-
-  res.json({ success: true, admin: user.admin });
+  if(!user) return res.status(401).json({error:"Login inválido"});
+  res.json({success:true, admin:user.admin});
 }
