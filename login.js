c993fdd265c528d0d8eee1232ad4139913c2fea0
diogodmrlk-crypto <diogodmrlk@gmail.com>
2026-02-05 15:57:36 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+export default function handler(req, res) {
+  const { username, password } = req.body;
+
+  const data = JSON.parse(fs.readFileSync("data.json"));
+
+  const user = data.users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (!user) return res.status(401).json({ error: "Login inválido" });
+
+  res.json({ success: true, admin: user.admin });
+}
